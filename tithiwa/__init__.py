37d75b1d2a08dcc33473daecf3715027d4df285e
maskgirl <tithimukherjee12@gmail.com>
2020-10-11 01:16:20 +0530

    removed tithiwa folder

--- tithiwa/__init__.py
+++
@@ -1,4 +0,0 @@
-from tithiwa.tithiwa import Tithiwa
-from session import Session
-from chatroom import Chatroom
-from group import Group
--- tithiwa/temp1.py
+++
@@ -1,8 +0,0 @@
-from tithiwa import Tithiwa
-
-tithiwabot = Tithiwa()
-tithiwabot.session.open_session("03")
-memberlist = tithiwabot.group.scrape_members_from_group("PROGRAMMING")
-print(memberlist)
-
-
