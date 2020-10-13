dd449458c4ab9b58fdc5902d43abfa25d3f16938
Vipul-Bajaj <vipulbjj@gmail.com>
2020-10-13 23:40:22 +0530

    Created send_message_with_mention_all_to_group()

--- tithiwa/tithiwa.py
+++ tithiwa/tithiwa.py
@@ -9,9 +9,9 @@ from waobject import WaObject
 class Tithiwa(WaObject):
     def __init__(self, browser=None):
         super().__init__(browser)
-        self.session = Session(self.browser)
-        self.chatroom = Chatroom(self.browser)
-        self.group = Group(self.browser)
+        self.session = Session(browser=self.browser)
+        self.chatroom = Chatroom(browser=self.browser)
+        self.group = Group(browser=self.browser)
 
 
     # def __del__(self):
