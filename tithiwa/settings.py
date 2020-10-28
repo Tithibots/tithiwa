926ded8617ac3300850e1df18585e168ca0b4f59
Navpreet Devpuri <navpreetdevpuri@gmail.com>
2020-10-28 14:20:17 +0530

    Update settings.py

--- tithiwa/settings.py
+++ tithiwa/settings.py
@@ -4,17 +4,17 @@ from constants import *
 class Settings():
     def open_settings(self):
         print('Opening settings', end="... ")
-        self._wait_for_an_element_to_be_clickable(SELECTORS.MAIN_MENU_OPTIONS.MENU_ICON).click()
-        self._wait_for_an_element_to_be_clickable(SELECTORS.MAIN_MENU_OPTIONS.SETTINGS).click()
+        self._wait_for_an_element_to_be_clickable(SELECTORS.MAIN_MENU_OPTIONS__MENU_ICON).click()
+        self._wait_for_an_element_to_be_clickable(SELECTORS.MAIN_MENU_OPTIONS__SETTINGS).click()
 
     def choose_theme(self, theme: str):
         self.open_settings()
         self._wait_for_an_element_to_be_clickable(
-            SELECTORS.SETTINGS.THEME).click()
+            SELECTORS.SETTINGS__THEME).click()
         self._wait_for_an_element_to_be_clickable(f'[value="{theme}"]').click()
         print(f'Applying {theme} theme', end="... ")
         self._wait_for_an_element_to_be_clickable(
-            SELECTORS.SETTINGS.OK_BUTTON).click()
+            SELECTORS.SETTINGS__OK_BUTTON).click()
         self.close_settings()
 
     def close_settings(self):
