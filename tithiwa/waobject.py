01e9d30491ace830f5a6f1d0737af959d7178272
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-10-31 23:21:48 +0530

    Update waobject.py

--- tithiwa/waobject.py
+++ tithiwa/waobject.py
@@ -49,15 +49,15 @@ class WaObject:
             return element
 
     def _wait_for_presence_of_all_elements(self, selector):
-        element = None
+        elements = None
         try:
-            element = WebDriverWait(self.browser, INTEGERS.DEFAULT_WAIT).until(
+            elements = WebDriverWait(self.browser, INTEGERS.DEFAULT_WAIT).until(
                 EC.presence_of_all_elements_located(selector)
             )
         except:
             pass
         finally:
-            return element
+            return elements
 
     def _wait_for_presence_of_an_element_in_other_element(self, selector, element):
         relement = None
