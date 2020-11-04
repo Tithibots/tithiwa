29b618694ebc19f41216741ef8ee8ffbaf5e74b7
Navpreet Singh Devpuri <navpreetdevpuri@gmail.com>
2020-11-04 21:26:06 +0530

    Update temp.py

--- tithiwa/temp.py
+++ tithiwa/temp.py
@@ -1,14 +1,12 @@
 from tithiwa import *
 
 tithiwabot = Tithiwa()
-# # tithiwabot.generate_session()
 tithiwabot.open_session()
 print("'" + tithiwabot.get_my_name() + "', '" + tithiwabot.get_my_about() + "'")
-tithiwabot.block(["alka", "amr", "9592140593"])
-# tithiwabot.setting_notifications(is_sounds=False, is_desktop_alerts=False, is_show_previews=False, is_turn_off_desktop_notifications=True)
-# tithiwabot.create_group("Group made by tithiwa", ["Navpreet Devpuri"])
-# tithiwabot.create_group("Group made by tithiwa", ["Navpreet Devpuri"])
-# tithiwabot.create_group("Group made by tithiwa", ["Navpreet Devpuri"])
-# tithiwabot.exit_from_all_groups()
-# tithiwabot.chatroom.send_message_to_number("919592140593", "Yess, from tithiwa")
+tithiwabot.quit()
+
+browser = webdriver.Chrome()
+tithiwabot = Tithiwa(browser)
+tithiwabot.open_session()
+print("'" + tithiwabot.get_my_name() + "', '" + tithiwabot.get_my_about() + "'")
 tithiwabot.quit()
