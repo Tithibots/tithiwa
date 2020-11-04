8ab3a3cc7a0cb88c9d126a107846b18c0efb23ac
Navpreet Singh Devpuri <navpreetdevpuri@gmail.com>
2020-11-04 22:32:22 +0530

    Update temp.py

--- tithiwa/temp.py
+++ tithiwa/temp.py
@@ -5,9 +5,10 @@ tithiwabot.open_session()
 print("'" + tithiwabot.get_my_name() + "', '" + tithiwabot.get_my_about() + "'")
 tithiwabot.quit()
 
-browser = webdriver.Chrome()
+browser = 3
 #doing something else with browser
 tithiwabot = Tithiwa(browser)
+tithiwabot.browser = webdriver.Chrome()
 tithiwabot.open_session()
 print("'" + tithiwabot.get_my_name() + "', '" + tithiwabot.get_my_about() + "'")
 tithiwabot.quit()
