8b226b70ad8ea0927be9e81323b53e0045d6efbb
Plamen Velkov <pyvelkov@myseneca.ca>
2020-12-02 16:54:11 -0500

    Finished and tested deleting all chats

--- tithiwa/contact.py
+++ tithiwa/contact.py
@@ -26,3 +26,34 @@ class Contact(Chatroom, WaObject):
             continue
         return number
 
+    def delete_chats_of_all_contacts(self, _shouldoutput=(True, True)):
+        self._wait_for_presence_of_an_element(SELECTORS.CONTACTS__NAME_IN_CHATS)
+        self._wait_for_an_element_to_be_clickable(SELECTORS.MAIN_SEARCH_BAR).click()
+        preactive = None
+        self.browser.switch_to.active_element.send_keys(Keys.ARROW_DOWN)
+        curractive = self.browser.switch_to.active_element
+        prechatname = None
+        while curractive != preactive:
+            chatnameelement = None
+            try:
+                chatnameelement = curractive.find_element(By.CSS_SELECTOR, SELECTORS.CONTACTS__NAME_IN_CHATS)
+            except:
+                pass
+            if chatnameelement != None:
+                chatname = chatnameelement.get_attribute('innerText')
+                self._wait_for_an_element_to_deattached(prechatname)
+                self._wait_for_chat_to_open(chatname)
+            preactive = curractive
+            prechatname = self._wait_for_presence_of_an_element(SELECTORS.CHATROOM__NAME)
+            curractive.send_keys(Keys.ARROW_DOWN)
+            curractive = self.browser.switch_to.active_element
+        # wait for options element to be available then click
+        self._wait_for_presence_of_an_element(SELECTORS.CHATROOM__OPTIONS)
+        self._wait_for_an_element_to_be_clickable(SELECTORS.CHATROOM__OPTIONS).click()
+        # wait for delete element to be available then click
+        self._wait_for_presence_of_an_element(SELECTORS.CHATROOM__INFO_DELETE_CHAT)
+        self._wait_for_an_element_to_be_clickable(SELECTORS.CHATROOM__INFO_DELETE_CHAT).click()
+        # wait for pop up to delete chat to be available then click
+        self._wait_for_presence_of_an_element(SELECTORS.CHATROOM__DELETE_CHAT)
+        self._wait_for_an_element_to_be_clickable(SELECTORS.CHATROOM__DELETE_CHAT).click()
+        self._close_info()
