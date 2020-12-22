9aa1d91f4c52b34a767418240cddb3b8f5bb5eb5
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-12-22 15:30:02 +0530

    correncted

--- tithiwa/temp.py
+++ tithiwa/temp.py
@@ -2,10 +2,11 @@ from tithiwa import *
 
 tithiwabot = Tithiwa()
 tithiwabot.generate_session()
+
 # tithiwabot.open_session()
 # print("'" + tithiwabot.get_my_name() + "', '" + tithiwabot.get_my_about() + "'")
-tithiwabot.remove_group_admins("Test", ["Navpreet Devpuri"])
-tithiwabot.quit()
+# tithiwabot.remove_group_admins("Test", ["Navpreet Devpuri"])
+# tithiwabot.quit()
 
 # browser = 3
 # #doing something else with browser
