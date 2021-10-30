406dbcbe5236ae553e215db283d6037d502387f3
Navpreet Singh Devpuri <navpreetdevpuri@gmail.com>
2021-10-30 13:50:33 +0530

    added CHATROOM__CLEAR_MESSAGES

--- tithiwa/constants.py
+++ tithiwa/constants.py
@@ -38,6 +38,7 @@ class SELECTORS(object):
     CHATROOM__OPTIONS = (By.CSS_SELECTOR, '#main span[data-testid="menu"]')
     CHATROOM__INFO_DELETE_CHAT = (By.CSS_SELECTOR, '._1OwwW ._3oTCZ[title="Delete chat"]')
     CHATROOM__DELETE_CHAT = (By.CSS_SELECTOR, 'li div[title="Delete chat"]')
+    CHATROOM__CLEAR_MESSAGES = (By.CSS_SELECTOR, 'li div[aria-label="Clear messages"]')
     CHATROOM__FOOTER = (By.CSS_SELECTOR, 'footer')
     CONTACTS__NAME_IN_CHATS = (By.CSS_SELECTOR, '#side div[aria-label^="Chat list"] div span span[dir="auto"][title]')
     GROUPS__MEMBERS_SEARCH_ICON = (By.CSS_SELECTOR, 'div[role="button"] span[data-testid="search"]')
